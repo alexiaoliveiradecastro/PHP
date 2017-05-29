7903963a8554daa69713e3bf697e8c9811c7f4f5
Aléxia Oliveira <oliveira.alexia2@gmail.com>
2017-05-28 22:01:08 -0300

    Create exemplo14_1.php

---
+++ exemplo14_1.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Exemplo 14.1</title>
+</head>
+  <body>
+    <h1>Exemplo 14.1</h1>
+  <?php
+    function testeLocal(){
+    $a=25;
+    }
+    echo"<h1>Variável Local</h1>";
+    testeLocal();
+    echo $a=25;
+  ?>
+  </body>
+</html>
